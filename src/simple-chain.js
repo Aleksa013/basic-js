b9917ba36d669ed62c78ef0527a410f5a4780c59
Aleksa013 <velma86@bk.ru>
2023-12-22 13:05:10 +0200

    feat: work with simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,18 +4,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+
+
 const chainMaker = {
-  getLength() {
-   
-  return Object.keys(chainMaker).length;
+  getLength() {  
+  this.length =  Object.keys(chainMaker).length-6;
+  return this.length
   },
-  addLink( value ) {
-    
-  this.position = this.getLength+1;
-  this.value= `(${value})`;
+  addLink(value) {  
+//  console.log(this.getLength());
+  this[this.getLength()] =  value.toString();
+  return chainMaker;
+  // this = `${this.value}`; 
+  // console.log(chainMaker[`${this.getLength()}`])
   },
-  removeLink( position ) {
-   
+  removeLink( position ) { 
+  this.position = position.toString();  
   delete this.position;
   },
   reverseChain() {
@@ -23,12 +27,21 @@ const chainMaker = {
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   let finish='';
+   for(let i = 0; i < this.getLength(); i++){
+    finish += `( ${this[i]} )~~`
+   }
+    return finish;
   }
 };
-
-
+chainMaker.getLength();
+chainMaker.addLink(12);
+console.log(chainMaker)
+// // chainMaker.addLink(1);
+// // chainMaker.getLength();
+// // chainMaker.removeLink(2);
+// chainMaker.finishChain();
+console.log(chainMaker.addLink(1).addLink(2).addLink(3).finishChain())
 
 module.exports = {
   chainMaker
